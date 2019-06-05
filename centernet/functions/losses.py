5efcd869d07c75d7732c1cbba565abdc670da0f5
higumachan <harekumo792154@gmail.com>
2019-06-05 12:06:12 +0900

    use **

--- centernet/functions/losses.py
+++ centernet/functions/losses.py
@@ -4,15 +4,14 @@ import numpy as np
 
 EPS = 2e-05
 
-def focial_loss(pred, gt, alpha=2, gamma=4, comm=None):
+def focial_loss(pred, gt, alpha=2, beta=4, comm=None):
     pos_indices = gt >= 1
     neg_indices = gt < 1
 
-    neg_weights = pow(1 - gt, gamma)
+    neg_weights = (1 - gt) ** beta
 
-
-    pos_loss = F.log(pred + EPS) * pow(1 - pred, alpha) * pos_indices
-    neg_loss = F.log(1 - pred + EPS) * pow(pred, alpha) * neg_weights * neg_indices
+    pos_loss = F.log(pred + EPS) * (1 - pred) ** alpha * pos_indices
+    neg_loss = F.log(1 - pred + EPS) * pred ** alpha * neg_weights * neg_indices
 
     num_pos = pos_indices.sum()
     pos_loss = F.sum(pos_loss)
