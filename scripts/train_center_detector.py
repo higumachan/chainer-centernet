7f2cabda79c8afdfda3f61f87f658b7a2d7eddbf
higumachan <harekumo792154@gmail.com>
2019-06-05 16:26:07 +0900

    implement mini mode

--- scripts/train_center_detector.py
+++ scripts/train_center_detector.py
@@ -1,6 +1,7 @@
 import argparse
 
 import chainer
+from chainer import datasets
 from chainer.datasets import TransformDataset, ConcatenatedDataset
 from chainer.iterators import MultiprocessIterator
 from chainer.optimizers import Adam
@@ -17,6 +18,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--batchsize', type=int, default=2)
+    parser.add_argument('--epoch', type=int, default=10)
+    parser.add_argument('--mini', action="store_true")
     args = parser.parse_args()
 
     num_class = len(voc_bbox_label_names)
@@ -30,11 +33,15 @@ def main():
         ),
         center_detection_transform
     )
+    if args.mini:
+        train = datasets.SubDataset(train, 0, 100)
     train_iter = chainer.iterators.MultiprocessIterator(train, args.batchsize)
 
     test = VOCBboxDataset(
         year='2007', split='test',
         use_difficult=True, return_difficult=True)
+    if args.mini:
+        test = datasets.SubDataset(test, 0, 20)
     test_iter = chainer.iterators.SerialIterator(
         test, args.batchsize, repeat=False, shuffle=False)
 
@@ -50,12 +57,16 @@ def main():
 
     updater = StandardUpdater(train_iter, optimizer, device=args.gpu)
 
-    log_interval = 100, 'iteration'
-    trainer = Trainer(updater, (12000, 'iteration'))
+    log_interval = 1, 'epoch'
+    trainer = Trainer(updater, (args.epoch, 'epoch'))
     trainer.extend(extensions.LogReport(trigger=log_interval))
     trainer.extend(extensions.observe_lr(), trigger=log_interval)
     trainer.extend(extensions.PrintReport(
-        ['epoch', 'iteration', 'lr', 'main/loss', 'main/hm_loss', 'main/wh_loss', 'main/offset_loss']),
+        [
+            'epoch', 'iteration', 'lr',
+            'main/loss', 'main/hm_loss', 'main/wh_loss', 'main/offset_loss',
+            'validation/main/map',
+        ]),
         trigger=log_interval)
     trainer.extend(extensions.ProgressBar(update_interval=10))
     trainer.extend(
@@ -63,7 +74,7 @@ def main():
             test_iter, detector, use_07_metric=True,
             label_names=voc_bbox_label_names),
         trigger=triggers.ManualScheduleTrigger(
-            100, 'iteration'))
+            1, 'epoch'))
 
     trainer.run()
 
